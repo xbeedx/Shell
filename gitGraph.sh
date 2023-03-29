2253fb922fde9fab2dcba25ab57e3903f5c00380
b.atrmouh <0xbeedx@gmail.com>
2023-03-29 15:39:56 +0200

    gitGraph

--- gitGraph.sh
+++ gitGraph.sh
@@ -1,5 +1,21 @@
-#!/bin/bash
-read -p "Commit description: " desc
-git add . 
-git commit -m "$desc" > ./log.txt
-grep '^\[.*' ./log.txt > graph.mk
+$branches = git branch --format="%(refname:short)"
+foreach ($branch in $branches) {
+     echo "Branch Name: $branch" > log.txt
+     $commits = git log $branch --format="%H %an %s"
+     echo "Commits:"  >> log.txt
+     echo $commits >> log.txt
+     echo " ``````mermaid `n`tgitGraph" >.\graph.md
+        If ($branch -ne "main"){
+            Write-Output "`tbranch $branch" >>.\graph.md
+        }
+     foreach($commit in $commits)
+     {
+         $commitFields = $commit.Split(' ')
+         $name = ""
+         for($i = 2; $i -lt $commitFields.Length; $i++) {
+            $name += $commitFields[$i] + " "
+        }
+         Write-Output "`tcommit id:`"$name`"" >>.\graph.md
+     }
+     echo "``````" >>.\graph.md
+ }
