c0e2b962ceb15d495d7f57d19e48ebd5d2b6d12d
b.atrmouh <0xbeedx@gmail.com>
2023-04-03 12:19:21 +0200

    fix

--- gitGraph.sh
+++ gitGraph.sh
@@ -1,21 +1,27 @@
 $branches = git branch --format="%(refname:short)"
-foreach ($branch in $branches) {
-     echo "Branch Name: $branch" > log.txt
-     $commits = git log $branch --format="%H %an %s"
-     echo "Commits:"  >> log.txt
-     echo $commits >> log.txt
-     echo " ``````mermaid `n`tgitGraph" >.\graph.md
-        If ($branch -ne "main"){
-            Write-Output "`tbranch $branch" >>.\graph.md
+$graphFilePath = ".\graph.md"
+$names = @()
+Clear-Content graph.md
+Clear-Content log.txt
+" ``````mermaid`n`tgitGraph" | Out-File -FilePath ".\graph.md" -Append
+for ($i = ($branches.Length - 1); $i -ge 0; $i--) {
+    $branch = $branches[$i]
+    "Branch Name: $branch `n Commits:" | Out-File -FilePath ".\log.txt" -Append
+    $commits = git log $branch --format="%H %an %s"
+    If ($branch -ne "main"){
+       "`tbranch $branch" | Out-File -FilePath ".\graph.md" -Append
+    }
+    for ($j = ($commits.Length - 1); $j -ge 0; $j--) {
+        $commit = $commits[$j]
+        $commitFields = $commit.Split(' ')
+        $name = ""
+        for($k = 2; $k -lt $commitFields.Length; $k++) {
+            $name += $commitFields[$k] + " "
         }
-     foreach($commit in $commits)
-     {
-         $commitFields = $commit.Split(' ')
-         $name = ""
-         for($i = 2; $i -lt $commitFields.Length; $i++) {
-            $name += $commitFields[$i] + " "
+        If (!($names -match $name)) {
+            "`tcommit id:`"$name`"" | Out-File -FilePath ".\graph.md" -Append
+            $names += $name
         }
-         Write-Output "`tcommit id:`"$name`"" >>.\graph.md
-     }
-     echo "``````" >>.\graph.md
+    }
  }
+ echo "``````" >>.\graph.md
